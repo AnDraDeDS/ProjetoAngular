0132823501090672abd4b18573855aeb8c7aeeee
Gabriel Andrade Vieira <101003038+AnDraDeDS@users.noreply.github.com>
2024-08-19 11:51:20 +0000

    App de calcular média

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
-}
+    constructor() { }
+    public notaA: number | undefined;
+    public notaB: number | undefined;
+    public media: number | undefined;
+    public situacao: string = "";
+    calcularMedia() {
+    if (this.notaA && this.notaB) {
+    this.media = (this.notaA + this.notaB) / 2;
+    this.situacao = this.media >= 7 ? 'Aprovado' : 'Reprovado';
+    }
+    }
+    }
