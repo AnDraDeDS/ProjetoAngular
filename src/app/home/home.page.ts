53f68c7da6579aa34e8fb9e3673c212c9ea523e2
Gabriel Andrade Vieira <101003038+AnDraDeDS@users.noreply.github.com>
2024-09-29 13:36:50 -0300

    Update home.page.ts
    ts da lista de generos musicais

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,24 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule], //CommonModule -> para funcionar o *ngFor
 })
 export class HomePage {
-    constructor() { }
-    public notaA: number | undefined;
-    public notaB: number | undefined;
-    public media: number | undefined;
-    public situacao: string = "";
-    calcularMedia() {
-    if (this.notaA && this.notaB) {
-    this.media = (this.notaA + this.notaB) / 2;
-    this.situacao = this.media >= 7 ? 'Aprovado' : 'Reprovado';
+  public nome = "";
+
+  public items: any[] = [ //any -> os itens podem ser qualquer tipo
+    {
+      titulo: "Forró"
+    },
+    {
+      titulo: "Rock"
+    },
+    {
+      titulo: "Sertanejo"
+    },
+    {
+      titulo: "Funk"
     }
-    }
-    }
+  ]
+
+  constructor() { }
+
+  adicionar() {
+    //Adicionando no array
+    this.items.push(
+      {
+        titulo: this.nome
+      }
+    );
+
+    //Limpando campo nome
+    this.nome = "";
+  }
+
+  remover(posicao:number) {
+    this.items.splice(posicao, 1);
+  }
+}
